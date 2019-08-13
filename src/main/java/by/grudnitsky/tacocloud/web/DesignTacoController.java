28c13c716f7eb5459b4a0307be4f9874bb322cb7
Nick Grudnitsky <grud97@gmail.com>
2019-08-13 10:36:18 +0300

    Spring Security

--- src/main/java/by/grudnitsky/tacocloud/web/DesignTacoController.java
+++ src/main/java/by/grudnitsky/tacocloud/web/DesignTacoController.java
@@ -4,8 +4,11 @@ package by.grudnitsky.tacocloud.web;
 import by.grudnitsky.tacocloud.Ingredient;
 import by.grudnitsky.tacocloud.Order;
 import by.grudnitsky.tacocloud.Taco;
+import by.grudnitsky.tacocloud.User;
 import by.grudnitsky.tacocloud.data.IngredientRepository;
 import by.grudnitsky.tacocloud.data.TacoRepository;
+import by.grudnitsky.tacocloud.data.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,7 @@ import by.grudnitsky.tacocloud.Ingredient.*;
 
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,17 +28,21 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("order")
+@Slf4j
 public class DesignTacoController {
 
     private final IngredientRepository ingredientRepository;
     private TacoRepository designRepo;
+    private UserRepository userRepo;
 
     @Autowired
     public DesignTacoController(
             IngredientRepository ingredientRepo,
-            TacoRepository designRepo) {
+            TacoRepository designRepo,
+            UserRepository userRepo) {
         this.ingredientRepository = ingredientRepo;
         this.designRepo = designRepo;
+        this.userRepo = userRepo;
     }
 
     @ModelAttribute(name = "order")
@@ -47,10 +55,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@Valid Taco taco, Errors errors, @ModelAttribute Order order){
+    public String processDesign(@Valid Taco taco, Errors errors
+            , @ModelAttribute Order order){
         if (errors.hasErrors()) {
             return "design";
         }
+
         Taco saved = designRepo.save(taco);
         order.addDesign(saved);
 
@@ -58,7 +68,8 @@ public class DesignTacoController {
     }
 
     @GetMapping
-    public String showDesignForm(Model model) {
+    public String showDesignForm(Model model, Principal principal) {
+        log.info("   --- Designing taco");
         List<Ingredient> ingredients = new ArrayList<>();
         ingredientRepository.findAll().forEach(ingredients::add);
         Type[] types = Type.values();
@@ -66,6 +77,10 @@ public class DesignTacoController {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
+
+        String username = principal.getName();
+        User user = userRepo.findByUsername(username);
+        model.addAttribute("user", user);
 
         return "design";
     }
--- src/main/java/by/grudnitsky/tacocloud/web/OrderController.java
+++ src/main/java/by/grudnitsky/tacocloud/web/OrderController.java
@@ -1,13 +1,12 @@
 package by.grudnitsky.tacocloud.web;
 
 import by.grudnitsky.tacocloud.Order;
+import by.grudnitsky.tacocloud.User;
 import by.grudnitsky.tacocloud.data.OrderRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
@@ -25,16 +24,35 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String orderForm(){
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute Order order){
+        if (order.getName() == null) {
+            order.setName(user.getFullname());
+        }
+        if (order.getStreet() == null) {
+            order.setStreet(user.getStreet());
+        }
+        if (order.getCity() == null) {
+            order.setCity(user.getCity());
+        }
+        if (order.getState() == null) {
+            order.setState(user.getState());
+        }
+        if (order.getZip() == null) {
+            order.setZip(user.getZip());
+        }
+
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus){
+    public String processOrder(@Valid Order order, Errors errors
+            , SessionStatus sessionStatus, @AuthenticationPrincipal User user){
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
+        order.setUser(user);
         orderRepo.save(order);
         sessionStatus.setComplete();
 
