f8b95ae74beed1b63e445fcfc1e84a4e63fb28a2
Nick Grudnitsky <grud97@gmail.com>
2019-08-15 18:39:01 +0300

    Rest Service

--- src/main/java/by/grudnitsky/tacocloud/data/IngredientRepository.java
+++
@@ -1,8 +0,0 @@
-package by.grudnitsky.tacocloud.data;
-
-import by.grudnitsky.tacocloud.Ingredient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-
-}
--- src/main/java/by/grudnitsky/tacocloud/data/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package by.grudnitsky.tacocloud.data;
-
-import by.grudnitsky.tacocloud.Taco;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-
-}
