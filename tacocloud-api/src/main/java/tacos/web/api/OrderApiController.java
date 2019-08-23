2e91d63452a88fbe9d3e6c4c583d3408fcacff63
Nick Grudnitsky <grud97@gmail.com>
2019-08-23 09:30:08 +0300

    Reactive REST

--- tacocloud-api/src/main/java/tacos/web/api/OrderApiController.java
+++ tacocloud-api/src/main/java/tacos/web/api/OrderApiController.java
@@ -14,8 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tacos.Order;
 import tacos.data.OrderRepository;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path="/orders",
@@ -23,66 +27,78 @@ import tacos.data.OrderRepository;
 @CrossOrigin(origins="*")
 public class OrderApiController {
 
-  /*private OrderRepository repo;
+    private OrderRepository repo;
 
-  public OrderApiController(OrderRepository repo) {
-    this.repo = repo;
-  }
-  
-  @GetMapping(produces="application/json")
-  public Iterable<Order> allOrders() {
-    return repo.findAll();
-  }
-  
-  @PostMapping(consumes="application/json")
-  @ResponseStatus(HttpStatus.CREATED)
-  public Order postOrder(@RequestBody Order order) {
-    return repo.save(order);
-  }
+    public OrderApiController(OrderRepository repo) {
+        this.repo = repo;
+    }
 
-  @PutMapping(path="/{orderId}", consumes="application/json")
-  public Order putOrder(@RequestBody Order order) {
-    return repo.save(order);
-  }
+    @GetMapping(produces="application/json")
+    public Flux<Order> allOrders() {
+        return repo.findAll();
+    }
 
-  @PatchMapping(path="/{orderId}", consumes="application/json")
-  public Order patchOrder(@PathVariable("orderId") Long orderId,
-                          @RequestBody Order patch) {
-    
-    Order order = repo.findById(orderId).get();
-    if (patch.getDeliveryName() != null) {
-      order.setDeliveryName(patch.getDeliveryName());
+//  @PostMapping(consumes="application/json")
+//  @ResponseStatus(HttpStatus.CREATED)
+//  public Mono<Order> postOrder(@RequestBody Mono<Order> order) {
+//    order.subscribe(orderMessages::sendOrder); // TODO: not ideal...work into reactive flow below
+//    return order
+//        .flatMap(repo::save);
+//  }
+
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Order> postOrder(@RequestBody Order order) {
+        return repo.save(order);
     }
-    if (patch.getDeliveryStreet() != null) {
-      order.setDeliveryStreet(patch.getDeliveryStreet());
+
+
+    @PutMapping(path="/{orderId}", consumes="application/json")
+    public Mono<Order> putOrder(@RequestBody Mono<Order> order) {
+        return order.flatMap(repo::save);
     }
-    if (patch.getDeliveryCity() != null) {
-      order.setDeliveryCity(patch.getDeliveryCity());
+
+    @PatchMapping(path="/{orderId}", consumes="application/json")
+    public Mono<Order> patchOrder(@PathVariable("orderId") UUID orderId,
+                                  @RequestBody Order patch) {
+
+        return repo.findById(orderId)
+                .map(order -> {
+                    if (patch.getDeliveryName() != null) {
+                        order.setDeliveryName(patch.getDeliveryName());
+                    }
+                    if (patch.getDeliveryStreet() != null) {
+                        order.setDeliveryStreet(patch.getDeliveryStreet());
+                    }
+                    if (patch.getDeliveryCity() != null) {
+                        order.setDeliveryCity(patch.getDeliveryCity());
+                    }
+                    if (patch.getDeliveryState() != null) {
+                        order.setDeliveryState(patch.getDeliveryState());
+                    }
+                    if (patch.getDeliveryZip() != null) {
+                        order.setDeliveryZip(patch.getDeliveryState());
+                    }
+                    if (patch.getCcNumber() != null) {
+                        order.setCcNumber(patch.getCcNumber());
+                    }
+                    if (patch.getCcExpiration() != null) {
+                        order.setCcExpiration(patch.getCcExpiration());
+                    }
+                    if (patch.getCcCVV() != null) {
+                        order.setCcCVV(patch.getCcCVV());
+                    }
+                    return order;
+                })
+                .flatMap(repo::save);
     }
-    if (patch.getDeliveryState() != null) {
-      order.setDeliveryState(patch.getDeliveryState());
+
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable("orderId") UUID orderId) {
+        try {
+            repo.deleteById(orderId);
+        } catch (EmptyResultDataAccessException e) {}
     }
-    if (patch.getDeliveryZip() != null) {
-      order.setDeliveryZip(patch.getDeliveryState());
-    }
-    if (patch.getCcNumber() != null) {
-      order.setCcNumber(patch.getCcNumber());
-    }
-    if (patch.getCcExpiration() != null) {
-      order.setCcExpiration(patch.getCcExpiration());
-    }
-    if (patch.getCcCVV() != null) {
-      order.setCcCVV(patch.getCcCVV());
-    }
-    return repo.save(order);
-  }
-  
-  @DeleteMapping("/{orderId}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteOrder(@PathVariable("orderId") Long orderId) {
-    try {
-      repo.deleteById(orderId);
-    } catch (EmptyResultDataAccessException e) {}
-  }*/
 
 }
--- tacocloud-api/src/main/java/tacos/web/api/RecentTacosController.java
+++ tacocloud-api/src/main/java/tacos/web/api/RecentTacosController.java
@@ -1,17 +1,9 @@
 package tacos.web.api;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
-
-import java.util.List;
-
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import reactor.core.publisher.Flux;
 import tacos.Taco;
 import tacos.data.TacoRepository;
 
@@ -24,21 +16,10 @@ public class RecentTacosController {
     this.tacoRepo = tacoRepo;
   }
 
-//  @GetMapping(path="/tacos/recent", produces="text/xml")
-//  public ResponseEntity<Resources<TacoResource>> recentTacos() {
-//    PageRequest page = PageRequest.of(
-//                          0, 12, Sort.by("createdAt").descending());
-//    List<Taco> tacos = tacoRepo.findAll(page).getContent();
-//
-//    List<TacoResource> tacoResources =
-//        new TacoResourceAssembler().toResources(tacos);
-//    Resources<TacoResource> recentResources =
-//            new Resources<TacoResource>(tacoResources);
-//
-//    recentResources.add(
-//        linkTo(methodOn(RecentTacosController.class).recentTacos())
-//            .withRel("recents"));
-//    return new ResponseEntity<>(recentResources, HttpStatus.OK);
-//  }
+  @GetMapping(path="/tacos/recent", produces="application/hal+json")
+  public Flux<Taco> recentTacos() {
+    return tacoRepo.findAll()
+            .take(12);
+  }
 
 }
